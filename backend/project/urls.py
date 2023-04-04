1d11a495c7dbe5250b7e06004783b01ae7cfe701
mdubelbeis <mason.dubelbeis@gmail.com>
2023-04-04 16:21:15 -0500

    set up init default routing

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -16,7 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import routers
+import notes.views
+import todos.views
+import expenses.views
+
+
+router = routers.DefaultRouter()
+router.register(r'todos', todos.views.TodoViewSet, 'todos')
+router.register(r'notes', notes.views.NoteViewSet, 'notes')
+router.register(r'expenses', expenses.views.ExpenseViewSet, 'expenses')
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("api.urls")),
+    path("api/", include(router.urls)),
 ]
