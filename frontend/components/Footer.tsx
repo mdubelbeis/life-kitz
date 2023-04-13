e7cb7a1bdeacf2ae1a5508263b42b9e851920479
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2023-04-13 10:43:34 -0500

    aded qr-code to portfolio and init styling

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,10 +1,16 @@
+import Image from "next/image";
+import qrCode from "../public/portfolio_qr_code.png";
+
 const Footer: React.FC = () => {
   return (
-    <footer>
+    <footer className="flex items-center justify-center gap-10 bg-black py-8 text-xl text-white">
       <small>
         Designed and developed by{" "}
-        <a href="https://masondubelbeis.com">Mason J. Dubelbeis, 2023</a>
+        <a href="https://masondubelbeis.com">
+          <span className="text-blue-300">Mason J. Dubelbeis</span>, 2023
+        </a>
       </small>
+      <Image src={qrCode} alt="QR code to portfolio" className="w-14" />
     </footer>
   );
 };
