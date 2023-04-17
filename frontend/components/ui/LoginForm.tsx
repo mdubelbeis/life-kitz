0ca1e9a6b57dcde0248fa7cf9d371b3e4b9006f0
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2023-04-17 18:22:38 -0500

    added icons

--- frontend/components/ui/LoginForm.tsx
+++ frontend/components/ui/LoginForm.tsx
@@ -1,26 +1,48 @@
 import Link from 'next/link';
-import AuthFormWrapper from '../auth/AuthFormWrapper';
+import { MdAlternateEmail, MdPassword } from 'react-icons/md';
 import Button from './Button';
 
-const LoginForm: React.FC = () => {
+interface LoginFormProps {
+  email: string;
+  accessToken: string;
+  password: string;
+  setEmail: (email: string) => void;
+  setPassword: (password: string) => void;
+}
+
+const LoginForm: React.FC<LoginFormProps> = ({
+  email,
+  accessToken,
+  password,
+  setEmail,
+  setPassword,
+}) => {
+  const handleLogin = () => {};
+
   return (
-    <AuthFormWrapper>
-      <label htmlFor="email">
+    <form className="flex flex-col gap-3" onSubmit={handleLogin}>
+      <label className="relative" htmlFor="email">
         <input
           type="email"
           id="email"
           placeholder="Email"
+          value={email}
           className="w-full rounded px-1 py-2 tracking-wider"
+          onChange={(e) => setEmail(e.target.value)}
         />
+        <MdAlternateEmail className="absolute right-2 top-1/2 -translate-y-1/2 transform text-gray-400" />
       </label>
 
-      <label htmlFor="password">
+      <label className="relative" htmlFor="password">
         <input
           type="password"
           id="password"
           placeholder="Password"
+          value={password}
           className="w-full rounded px-1 py-2 tracking-wider"
+          onChange={(e) => setPassword(e.target.value)}
         />
+        <MdPassword className="absolute right-2 top-1/2 -translate-y-1/2 transform text-gray-400" />
       </label>
 
       <div className="flex items-center justify-center gap-3">
@@ -29,7 +51,7 @@ const LoginForm: React.FC = () => {
         </Link>
         <Button type="submit">Login</Button>
       </div>
-    </AuthFormWrapper>
+    </form>
   );
 };
 
