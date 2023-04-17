2eec99db0a6137c6e7c79d76ddbdceec4e59b110
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2023-04-17 18:20:58 -0500

    removed

--- frontend/components/auth/AuthFormWrapper.tsx
+++
@@ -1,9 +0,0 @@
-interface AuthFormWrapperProps {
-  children: React.ReactNode;
-}
-
-const AuthFormWrapper: React.FC<AuthFormWrapperProps> = ({ children }) => {
-  return <form className="flex flex-col gap-3">{children}</form>;
-};
-
-export default AuthFormWrapper;
