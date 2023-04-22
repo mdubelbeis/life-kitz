8e49ced7b12930465065aac12d9df156f23861dc
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2023-04-22 14:39:59 -0500

    created possible Todo Form component

---
+++ frontend/components/todos/TodoForm.tsx
@@ -0,0 +1,11 @@
+import { Todo } from '@/pages';
+
+interface TodoFormProps {
+  todos: Todo[];
+}
+
+const TodoForm: React.FC<TodoFormProps> = ({}) => {
+  return <div>TodoForm</div>;
+};
+
+export default TodoForm;
