e57275601a99258be4fec4b6c5e0e8a5883fd7d4
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2023-04-22 16:01:06 -0500

    utilized the TodoForm for clean code

--- frontend/components/todos/TodoForm.tsx
+++ frontend/components/todos/TodoForm.tsx
@@ -1,11 +1,54 @@
-import { Todo } from '@/pages';
+import Link from 'next/link';
 
 interface TodoFormProps {
-  todos: Todo[];
+  newTodoTitle: string;
+  newTodoDescription: string;
+  setNewTodoTitle: React.Dispatch<React.SetStateAction<string>>;
+  setNewTodoDescription: React.Dispatch<React.SetStateAction<string>>;
+  handleNewTodo: (e: React.FormEvent<HTMLFormElement>) => Promise<void>;
 }
 
-const TodoForm: React.FC<TodoFormProps> = ({}) => {
-  return <div>TodoForm</div>;
+const TodoForm: React.FC<TodoFormProps> = ({
+  handleNewTodo,
+  newTodoTitle,
+  setNewTodoTitle,
+  newTodoDescription,
+  setNewTodoDescription,
+}) => {
+  return (
+    <form className="flex flex-col gap-3" onSubmit={handleNewTodo}>
+      <label>
+        <input
+          type="text"
+          placeholder="Todo Title..."
+          className="input w-full rounded-md border-2 border-gray-300"
+          id="title"
+          name="title"
+          value={newTodoTitle}
+          onChange={(e) => setNewTodoTitle(e.target.value)}
+        />
+      </label>
+      <label>
+        <input
+          type="text"
+          placeholder="Type here"
+          className="input w-full rounded-md border-2 border-gray-300"
+          id="description"
+          name="description"
+          value={newTodoDescription}
+          onChange={(e) => setNewTodoDescription(e.target.value)}
+        />
+      </label>
+      <div className="flex gap-3">
+        <button type="submit" className="btn-primary btn">
+          Add Todo
+        </button>
+        <button className="btn-secondary btn">
+          <Link href="/todos">Cancel</Link>
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default TodoForm;
