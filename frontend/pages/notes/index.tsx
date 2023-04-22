ea20295789e2cd7b135f6b0dc97ab9346edcea57
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2023-04-22 17:36:52 -0500

    fixed logic for fetching data and POST request

--- frontend/pages/notes/index.tsx
+++ frontend/pages/notes/index.tsx
@@ -1,3 +1,5 @@
+import NotesDisplay from '@/components/notes/NotesDisplay';
+import NotesForm from '@/components/notes/NotesForm';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
@@ -5,19 +7,26 @@ import { Note } from '../index';
 
 const NotesPage = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [notes, setNotes] = useState<Note[]>([]);
+  const [notes, setNotes] = useState<Note[] | null>(null);
+  const [newNoteTitle, setNewNoteTitle] = useState<string>('');
+  const [newNoteContent, setNewNoteContent] = useState<string>('');
   const router = useRouter();
 
   const getNotes = async () => {
     try {
-      const notes_data = await fetch('http://localhost:8000/api/notes/');
+      const notes_data = await fetch('http://localhost:8000/api/notes/', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token').slice(1, -1)}`,
+        },
+      });
+
       const notes_json = await notes_data.json();
       const notes = notes_json;
 
       if (notes) {
         setNotes(notes);
       } else {
-        setNotes([]);
+        setNotes(null);
       }
     } catch (error) {
       console.log(error + 'Error fetching data from /api/notes/');
@@ -34,6 +43,34 @@ const NotesPage = () => {
     }
   }, []);
 
+  const handleNewNote = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const newNote = {
+      title: newNoteTitle,
+      content: newNoteContent,
+    };
+
+    try {
+      const response = await fetch('http://localhost:8000/api/notes/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('token').slice(1, -1)}`,
+        },
+        body: JSON.stringify(newNote),
+      });
+
+      const data = await response.json();
+      console.log(data);
+      await getNotes();
+    } catch (error) {
+      console.log(error + 'Error creating new note');
+    }
+    setNewNoteTitle('');
+    setNewNoteContent('');
+  };
+
   return (
     <>
       <Head>
@@ -43,21 +80,16 @@ const NotesPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {isAuthenticated ? (
-        <>
-          <h1>Notes</h1>
-          <ul>
-            {!notes ? (
-              notes.map((note) => (
-                <li key={note.id}>
-                  <h2>{note.title}</h2>
-                  <p>{note.content}</p>
-                </li>
-              ))
-            ) : (
-              <p>No notes yet</p>
-            )}
-          </ul>
-        </>
+        <section className="mx-auto flex w-11/12 max-w-6xl flex-col gap-10">
+          <NotesForm
+            newNoteTitle={newNoteTitle}
+            newNoteContent={newNoteContent}
+            setNewNoteContent={setNewNoteContent}
+            setNewNoteTitle={setNewNoteTitle}
+            handleNewNote={handleNewNote}
+          />
+          <NotesDisplay notes={notes} />
+        </section>
       ) : (
         <p>Loading...</p>
       )}
