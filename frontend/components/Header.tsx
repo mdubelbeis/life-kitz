970502e0c9dc7b2aeb4984b06dd0a916623ce790
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2023-04-22 16:31:57 -0500

    fixed header logic - isAuth

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,39 +1,54 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 const Header: React.FC = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const router = useRouter();
   const { push, pathname } = useRouter();
 
   const handleLogout = () => {
+    setIsAuthenticated(false);
     localStorage.removeItem('token');
     localStorage.removeItem('refreshToken');
     localStorage.removeItem('isAuthenticated');
     push('/login');
   };
 
+  useEffect(() => {
+    // Check if user is authenticated
+    if (localStorage.getItem('token')) {
+      setIsAuthenticated(true);
+    } else {
+      router.push('/login');
+    }
+  });
+
   return (
     <header>
-      Under construction
       <h1 className="w-6/12">Life-Kitz</h1>
-      <nav className="flex w-full justify-between">
-        <div>
-          <Link href="/todos">Todos</Link>
-          <Link href="/expenses">Expenses</Link>
-          <Link href="/notes">Notes</Link>
-        </div>
-        <span onClick={handleLogout}>Logout</span>
-      </nav>
-      <nav>
-        {pathname === '/login' ? (
-          <Link href="/signup" className="text-blue-700 underline">
-            Sign up
-          </Link>
-        ) : (
-          <Link href="/login" className="text-blue-700 underline">
-            Log in
-          </Link>
-        )}
-      </nav>
+      {!isAuthenticated ? (
+        <nav>
+          {pathname === '/login' ? (
+            <Link href="/signup" className="text-blue-700 underline">
+              Sign up
+            </Link>
+          ) : (
+            <Link href="/login" className="text-blue-700 underline">
+              Log in
+            </Link>
+          )}
+        </nav>
+      ) : (
+        <nav className="flex w-full justify-between">
+          <div>
+            <Link href="/todos">Todos</Link>
+            <Link href="/expenses">Expenses</Link>
+            <Link href="/notes">Notes</Link>
+          </div>
+          <span onClick={handleLogout}>Logout</span>
+        </nav>
+      )}
     </header>
   );
 };
