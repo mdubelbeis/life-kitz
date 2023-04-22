0d60f84cbad751771160128356859b495e442a82
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2023-04-22 17:37:29 -0500

    added notes components

---
+++ frontend/components/notes/NotesDisplay.tsx
@@ -0,0 +1,43 @@
+import { Note } from '@/pages';
+
+interface NotesDisplayProps {
+  notes: Note[];
+}
+
+const NotesDisplay: React.FC<NotesDisplayProps> = ({ notes }) => {
+  return (
+    <>
+      {notes ? (
+        <div className="overflow-x-auto">
+          <table className="table-zebra table w-full">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Content</th>
+                <th>Quick Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {notes?.map((note) => (
+                <tr key={note.id}>
+                  <td>{note.title}</td>
+                  <td className="line-clamp-1">{note.content}</td>
+                  <td>
+                    <button className="mr-2">Completed</button>
+                    <button className="mr-2">Delete</button>
+                    <button>Edit</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <p>No notes</p>
+      )}
+    </>
+  );
+};
+
+export default NotesDisplay;
---
+++ frontend/components/notes/NotesForm.tsx
@@ -0,0 +1,53 @@
+import Link from 'next/link';
+
+interface NotesFormProps {
+  newNoteTitle: string;
+  newNoteContent: string;
+  setNewNoteTitle: React.Dispatch<React.SetStateAction<string>>;
+  setNewNoteContent: React.Dispatch<React.SetStateAction<string>>;
+  handleNewNote: (e: React.FormEvent<HTMLFormElement>) => Promise<void>;
+}
+
+const NotesForm: React.FC<NotesFormProps> = ({
+  newNoteTitle,
+  newNoteContent,
+  setNewNoteTitle,
+  setNewNoteContent,
+  handleNewNote,
+}) => {
+  return (
+    <form onSubmit={handleNewNote} className="flex flex-col gap-3">
+      <label>
+        <input
+          type="text"
+          placeholder="Title"
+          className="input w-full rounded-md border-2 border-gray-300"
+          id="title"
+          name="title"
+          value={newNoteTitle}
+          onChange={(e) => setNewNoteTitle(e.target.value)}
+        />
+      </label>
+      <label>
+        <textarea
+          placeholder="Content"
+          className="input w-full rounded-md border-2 border-gray-300"
+          id="content"
+          name="content"
+          value={newNoteContent}
+          onChange={(e) => setNewNoteContent(e.target.value)}
+        />
+      </label>
+      <div className="flex items-center justify-center gap-3">
+        <button type="submit" className="btn-primary btn">
+          Add Note
+        </button>
+        <button className="btn-secondary btn">
+          <Link href="/notes">Cancel</Link>
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default NotesForm;
